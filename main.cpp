82780c9aba208d42e98123fd8fc07086797592f6
Devlin <devlinpapworth@gmail.com>
2025-03-18 16:22:14 +0000

    comments to main

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(void)
 	ask_usr user;
 	data_handle data;
 	Error_check check;
-	IDW func;// calling the classes in the main function
+	IDW func;// calling the classes into main function
 	
 	
 			
@@ -33,17 +33,18 @@ int main(void)
 
 	int num_DH = user.get_num_DH();
 
-	double** input_array = data.Create_DH_array(num_DH, grid_x, grid_y);
+	double** input_array = data.Create_DH_array(num_DH, grid_x, grid_y); // creating the array to store the data
 
-	input_array = check.all_check(grid_x, grid_y, input_array, num_DH);
+	input_array = check.all_check(grid_x, grid_y, input_array, num_DH); // checking all the data before running the IDW function
 
 	cout << "\n.............................................\n";
-	func.IDW_top_func(grid_x, grid_y, power, input_array, num_DH); 
+	func.IDW_top_func(grid_x, grid_y, power, input_array, num_DH);  // calling the IDW function
 	cout << "\n.............................................\n";
 	
 	run_py();
 
 	cout << "\n IDW model is complete attempting to clear data \n";
+
 	for (int i = 0; i < num_DH; i++)
 	{
 		delete[] input_array[i];
